d92097bcccf8fb54211c7306df331ec8f263a7fa
viti04ek <99684929+viti04ek@users.noreply.github.com>
2023-06-12 23:36:16 +0300

    Controlling animation
    CHANGED:
    - script PlayerController

--- MobilePlatformer/Assets/Scripts/PlayerController.cs
+++ MobilePlatformer/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,9 @@ public class PlayerController : MonoBehaviour
 
     public SpriteRenderer SpriteRenderer;
 
+    public Animator Animator;
+    private bool _isJumping;
+
 
     private void Start()
     {
@@ -27,6 +30,8 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetButtonDown("Jump"))
             Jump();
+
+        ShowFalling();
     }
 
 
@@ -38,17 +43,43 @@ public class PlayerController : MonoBehaviour
             SpriteRenderer.flipX = true;
         else if (playerSpeed > 0)
             SpriteRenderer.flipX = false;
+
+        if (!_isJumping)
+            Animator.SetInteger("State", 1);
     }
 
 
     private void StopMoving()
     {
         Rigidbody2D.velocity = new Vector2(0, Rigidbody2D.velocity.y);
+
+        if (!_isJumping)
+            Animator.SetInteger("State", 0);
     }
 
 
     private void Jump()
     {
+        _isJumping = true;
         Rigidbody2D.AddForce(new Vector2(0, JumpSpeed));
+        Animator.SetInteger("State", 2);
+    }
+
+
+    private void ShowFalling()
+    {
+        if (Rigidbody2D.velocity.y<0)
+        {
+            Animator.SetInteger("State", 3);
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isJumping = false;
+        }
     }
 }
