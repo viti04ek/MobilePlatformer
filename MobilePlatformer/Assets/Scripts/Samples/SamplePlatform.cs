78a9cff72addca56c22f29e89a7f1de61a461f17
viti04ek <99684929+viti04ek@users.noreply.github.com>
2023-06-16 01:26:51 +0300

    Creating level
    ADDED:
    - script SamplePlatform

---
+++ MobilePlatformer/Assets/Scripts/Samples/SamplePlatform.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Controls the platform's dropping behavior
+/// </summary>
+public class SamplePlatform : MonoBehaviour 
+{
+	public float droppingDelay;
+
+	Rigidbody2D rb;
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D>();	
+	}
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			Invoke("StartDropping", droppingDelay);
+		}
+	}
+
+
+    void StartDropping()
+	{
+		rb.isKinematic = false;
+	}
+
+}
